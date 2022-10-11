27dad955eb4852b6c0a6e9732968d9484742b5a7
arobertapinheiro <arobertapinheiro@gmail.com>
2022-10-11 15:21:02 -0300

    Initial Commit

---
+++ AulaPG/AulaPGApp.swift
@@ -0,0 +1,17 @@
+//
+//  AulaPGApp.swift
+//  AulaPG
+//
+//  Created by Roberta Pinheiro on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct AulaPGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AulaPG/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AulaPG
+//
+//  Created by Roberta Pinheiro on 11/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
